9cc041f69c3b9a6679249a07214ea75d9a90435e
Tusar Ahmed <40721215+Tusar-A@users.noreply.github.com>
2023-10-05 14:16:14 +0600

    Add files via upload

---
+++ inserting_data_into_db.sql
@@ -0,0 +1,30 @@
+insert into employees
+(
+	emp_no,
+    birth_date,
+    first_name,
+    last_name,
+    gender,
+    hire_date
+)
+values (
+	999901,
+    '1997-03-17',
+    'john',
+    'smith',
+    'M',
+    '2024-09-01'
+);
+
+update employees
+set first_name = 'Stella',
+	last_name = 'parkinson'
+where emp_no = 999901;
+
+select *
+from employees
+where emp_no = 999901;
+
+-- commit;
+delete from employees
+where emp_no = 999901;
