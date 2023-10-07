ee3159ceab032db930cb29ea1b2aecac620fc759
Tusar Ahmed <40721215+Tusar-A@users.noreply.github.com>
2023-10-07 12:07:48 +0600

    Add files via upload

---
+++ union_in_mysql.sql
@@ -0,0 +1,41 @@
+# Assign employee number 110022 as a manager to all employees from 10001 to 10020, and employee number 110039 as manager to all
+# employees from 10021 to 10040
+SELECT 
+    a.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_id,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A 
+UNION SELECT 
+    b.*
+FROM
+    (SELECT 
+        e.emp_no AS employee_id,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS B;
